6abed0a79bea85f7f4b59892c9ae44b4af02f79c
PC <PC@DESKTOP-MGNCE95>
2021-06-03 23:33:38 -0300

    COMMIT 41

---
+++ src/view/Principal.java
@@ -0,0 +1,29 @@
+package view;
+
+import java.io.IOException;
+
+import controller.ArquivosController;
+import controller.IArquivosController;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		IArquivosController arqCont = new ArquivosController();
+		String dirWin = "C:\\generic_food";
+		String path =  "C:\\generic_food";
+		String nome = "fruta.txt";
+		
+		try {
+//			arqCont.readDir(dirWin);
+//        	arqCont.createFile(path,nome);
+//			arqCont.readFile(path,nome);
+			arqCont.openFile(path,nome);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+
+	}
+
+}
